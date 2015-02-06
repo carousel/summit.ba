4c52883fea1bdac288634089338048aa9e87646b
Miroslav Trninic <miroslav.trninic@gmail.com>
2015-02-06 09:23:14 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Summit.ba</title>
+        <link rel = "stylesheet" href= "">
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+        <style>
+
+            body {
+                margin:0;
+                text-align:center;
+            }
+            a{
+                color:red;
+            }
+            .welcome{
+                margin:30px;
+            }
+
+            </style>
+    </head>
+    <body>
+            <div class="welcome">
+                <img src="http://summit.ba/images/header-logo.jpg" alt="summit-logo"/>
+                <h3>Aplikacija je trenutno u pripremi.</h3>
+                <h3>Za dodatne informacije kontaktirajte nas na <a href ="mailto:miroslav.trninic@gmail.com">email</a></h3>
+                <h3>Vidimo se uskoro.</h3>
+                                                
+            </div>
+        <script src="//code.jquery.com/jquery.js"></script>
+    </body>
+</html>
